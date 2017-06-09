bf16efe6311a99f8b7586b0ad3cc70077ca50fbf
thesottishmonk <amitsingh.chouhan@live.com>
2017-06-09 15:54:25 +0530

    Add files via upload

---
+++ api.php
@@ -0,0 +1,12 @@
+<?php
+$hkurl = "http://api.hotukdeals.com/rest_api/v2/?key=370830551f30f20b3eb0088308f622c4&output=json&online_offline=online&order=new&search=3gb%20data%20ee%20vodafone%20three";
+        $service_url     = $hkurl ;
+        $curl            = curl_init($service_url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $curl_response   = curl_exec($curl);
+        curl_close($curl);
+        echo($curl_response);
+
+?>
