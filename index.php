cf69faa7d5d4051a876428eaf41fbcf79ba644d8
thesottishmonk <amitsingh.chouhan@live.com>
2019-10-16 17:47:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo("HELLO WORLD");
-echo("Testing this script")
+echo("Testing this script");
 $hkurl = "http://api.hotukdeals.com/rest_api/v2/?key=370830551f30f20b3eb0088308f622c4&output=json&online_offline=online&order=new&search=3gb%20data%20ee%20vodafone%20three";
         $service_url     = $hkurl ;
         $curl            = curl_init($service_url);
